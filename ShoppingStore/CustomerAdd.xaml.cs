758be7e475882781b649d512b81c0ed7f144784c
exceedinglife <extremelyexceeding@gmail.com>
2018-10-17 22:48:12 -0500

    Customer Screen xaml finished
    Created CustomerAdd.xaml, Grid WPF layout, form GUI layout
    (SOMEHOW LOST "LoginScreen.xaml" in my project on visual studio. will not open in project solution explorer but shows the file in file explorer. So created a LoginWindow.xaml duplicate.

---
+++ ShoppingStore/CustomerAdd.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ShoppingStore
+{
+    /// <summary>
+    /// Interaction logic for CustomerAdd.xaml
+    /// </summary>
+    public partial class CustomerAdd : Window
+    {
+        public CustomerAdd()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCreateCustomer_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnAdminScreen_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCustomerList_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
